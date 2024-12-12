ae665f02284ee98bfb6de050d7c638c45ff93a30
jhonatan.godoi <jhonatangodoi@hotmail.com>
2024-12-12 14:09:54 -0300

    refactor: remove useless route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
 import { TaskListComponent } from './components/task-list/task-list.component';
-import { CreateTaskComponent } from './components/create-task/create-task.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: TaskListComponent
-  },
-  {
-    path: 'create-task',
-    component: CreateTaskComponent
   }
 ];
